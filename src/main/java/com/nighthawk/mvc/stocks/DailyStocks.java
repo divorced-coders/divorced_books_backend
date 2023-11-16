a9f3bcecfc34a729b7d8892527a6d4ef87830f8b
firestrom0986 <firestorm0986@gmail.com>
2023-11-16 12:00:05 -0800

    done with the set up

--- src/main/java/com/nighthawk/mvc/stocks/DailyStocks.java
+++ src/main/java/com/nighthawk/mvc/stocks/DailyStocks.java
@@ -1,5 +1,61 @@
 package com.nighthawk.mvc.stocks;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.TypeDef;
+
+import com.mongodb.lang.NonNull;
+import com.vladmihalcea.hibernate.type.json.JsonType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+Person is a POJO, Plain Old Java Object.
+First set of annotations add functionality to POJO
+--- @Setter @Getter @ToString @NoArgsConstructor @RequiredArgsConstructor
+The last annotation connect to database
+--- @Entity
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@TypeDef(name="json", typeClass = JsonType.class)
+
 public class DailyStocks {
-  
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NonNull
+    private String open;
+
+    @NonNull
+    private String close;
+
+    @NonNull
+    private String high;
+
+    @NonNull
+    private String low;
+
+    @NonNull
+    private String volume;
+
+    @NonNull
+    private String symbol;
+
+    public DailyStocks(String open, String close, String high, String low, String volume, String symbol){
+        this.open = open;
+        this.close = close;
+        this.high = high;
+        this.low = low;
+        this.volume = volume;
+        this.symbol = symbol;
+    }
 }
--- src/main/java/com/nighthawk/mvc/stocks/MonthlyStocks.java
+++ src/main/java/com/nighthawk/mvc/stocks/MonthlyStocks.java
@@ -1,5 +1,76 @@
 package com.nighthawk.mvc.stocks;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.TypeDef;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.mongodb.lang.NonNull;
+import com.vladmihalcea.hibernate.type.json.JsonType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Collection;
+
+import static javax.persistence.FetchType.EAGER;
+
+/*
+Person is a POJO, Plain Old Java Object.
+First set of annotations add functionality to POJO
+--- @Setter @Getter @ToString @NoArgsConstructor @RequiredArgsConstructor
+The last annotation connect to database
+--- @Entity
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@TypeDef(name="json", typeClass = JsonType.class)
+
 public class MonthlyStocks {
-  
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NonNull
+    private String open;
+
+    @NonNull
+    private String close;
+
+    @NonNull
+    private String high;
+
+    @NonNull
+    private String low;
+
+    @NonNull
+    private String volume;
+
+    @NonNull
+    private String symbol;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    @ManyToMany(fetch = EAGER)
+    private Collection<DailyStocks> daily = new ArrayList<>();
+
+    public MonthlyStocks(String open, String close, String high, String low, String volume, String symbol, Date date){
+        this.open = open;
+        this.close = close;
+        this.high = high;
+        this.low = low;
+        this.volume = volume;
+        this.symbol = symbol;
+        this.date = date;   
+    }
 }
